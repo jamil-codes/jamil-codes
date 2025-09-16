e74f2fce123e56ba51142e1b65cbc4688cf1881f
Jamil Ahmed <jamilcodes@gmail.com>
2025-09-16 16:39:42 +0500

    Uploaded Portfolio made with next js static export

---
+++ _next/static/chunks/76bd93a8d2b959c2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/5a6c3db863a354ce.js",
+  "static/chunks/f6ad978b70e339ce.js",
+  "static/chunks/turbopack-debafafc625feeb3.js"
+])
---
+++ _next/static/chunks/e1abcb6e3a1f803b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/6b297c6bc770653d.js",
+  "static/chunks/f6ad978b70e339ce.js",
+  "static/chunks/turbopack-216a06d5584778dc.js"
+])
